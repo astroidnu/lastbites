f7a4cedc14920c104ccdf698177bf55486d9e733
Ibnu Muzzakkir <ibnu.muzzakkir@amarbank.co.id>
2019-01-03 08:33:57 +0700

    add bottom navigation

---
+++ app/src/main/java/com/lastbites/presentation/dashboard/DashboardActivity.kt
@@ -0,0 +1,17 @@
+package com.lastbites.presentation.dashboard
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.lastbites.R
+
+/**
+ * Created by ibnumuzzakkir on 03/01/19.
+ * Mobile Engineer
+ */
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
